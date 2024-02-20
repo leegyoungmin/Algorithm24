e0d8e3e124736e122a1fc65bc6a15a9de24aae6e
leegyoungmin <cow970814@naver.com>
2024-02-20 18:20:30 +0900

    feat : 240211

--- main/240211/수정렬하기4.swift
+++ main/240211/수정렬하기4.swift
@@ -1,53 +1,53 @@
 // https://www.acmicpc.net/problem/11931
 
-import Foundation
-
-final class FileIO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-            var sum = 0
-            var now = read()
-            var isPositive = true
-
-            while now == 10
-                || now == 32 { now = read() } // 공백과 줄바꿈 무시
-            if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
-            while now >= 48, now <= 57 {
-                sum = sum * 10 + Int(now-48)
-                now = read()
-            }
-
-            return sum * (isPositive ? 1:-1)
-        }
-}
-
-let file = FileIO()
-
-let cnt = file.readInt()
-
-var array = [Int]()
-
-for _ in 0..<cnt {
-  let number = file.readInt()
-  array.append(number)
-}
-
-array.sorted(by: >).forEach {
-  print($0)
-}
+//import Foundation
+//
+//final class FileIO {
+//    private let buffer:[UInt8]
+//    private var index: Int = 0
+//
+//    init(fileHandle: FileHandle = FileHandle.standardInput) {
+//        
+//        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+//    }
+//
+//    @inline(__always) private func read() -> UInt8 {
+//        defer { index += 1 }
+//
+//        return buffer[index]
+//    }
+//
+//    @inline(__always) func readInt() -> Int {
+//            var sum = 0
+//            var now = read()
+//            var isPositive = true
+//
+//            while now == 10
+//                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+//            if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+//            while now >= 48, now <= 57 {
+//                sum = sum * 10 + Int(now-48)
+//                now = read()
+//            }
+//
+//            return sum * (isPositive ? 1:-1)
+//        }
+//}
+//
+//let file = FileIO()
+//
+//let cnt = file.readInt()
+//
+//var array = [Int]()
+//
+//for _ in 0..<cnt {
+//  let number = file.readInt()
+//  array.append(number)
+//}
+//
+//array.sorted(by: >).forEach {
+//  print($0)
+//}
 
 //func mergeSort(_ array: [Int]) -> [Int] {
 //  if array.count <= 1 { return array }
